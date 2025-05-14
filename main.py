7e3a1e2557c28b4d47c69532aeadfdbf1d87334c
ru <rubygaming6@gmail.com>
2025-05-14 16:14:39 -0400

    implemented simple game loop that draws screen as black

--- main.py
+++ main.py
@@ -13,10 +13,6 @@ def main():
         pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
 
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
 
 if __name__ == "__main__":
     main()

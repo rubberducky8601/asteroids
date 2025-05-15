41490ab3c73b933544e2eb24d0aedf5a06423728
ru <rubygaming6@gmail.com>
2025-05-14 22:38:03 -0400

    player implementation

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 import pygame
+from pygame.sprite import Group
 
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+
+    dt = 0
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    Player.containers = (updatables, drawables)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0, 0, 0))
+        updatables.update(dt)
+
+        screen.fill("black")
+        for drawable in drawables:
+            drawable.draw(screen)
         pygame.display.flip()
+
+        dt = (clock.tick(60)) / 1000
 
 
 if __name__ == "__main__":
